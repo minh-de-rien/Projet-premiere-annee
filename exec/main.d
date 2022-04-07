5838987503ab4578f92445a0a32b7750933ca40c
minh-de-rien <77995908+minh-de-rien@users.noreply.github.com>
2022-04-07 02:14:04 -0400

    add gitignore

--- exec/main.d
+++
@@ -1,6 +0,0 @@
-main.o: main.cpp ../lib/robot.h ../lib/moteur.h ../lib/controleDelClass.h \
- ../lib/son.h ../lib/minuterie_1.h ../lib/lcd.h \
- ../lib/lcm_so1602dtr_m_fw.h ../lib/customprocs.h ../lib/minuterie_2.h \
- ../lib/initialisationBouton.h ../lib/boutonBlanc.h \
- ../lib/initialisationBouton.h ../lib/capteurDistance.h ../lib/can.h \
- ../lib/uart.h
